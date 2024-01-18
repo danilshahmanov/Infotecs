e81ecf74f25de5c36af7b5a7119312a50a705feb
danilshahmanov <daniltopskij174@gmail.com>
2024-01-19 00:08:11 +0300

    update tests

--- TestInfotecs/ControllerTest.cs
+++ TestInfotecs/ControllerTest.cs
@@ -6,6 +6,7 @@ using Moq;
 using Xunit;
 using Infotecs.Source.Controllers;
 using Infotecs.Source.Services;
+using Infotecs.Source.Data.Models;
 namespace TestInfotecs
 {
     public class ExperimentControllerTests
@@ -16,51 +17,39 @@ namespace TestInfotecs
 
         public ExperimentControllerTests()
         {
-            // Mock the dependencies
             _mockExperimentService = new Mock<IExperimentService>();
             _controller = new ExperimentController(_mockExperimentService.Object);
             _mockFormFile = new Mock<IFormFile>();
         }
 
         [Fact]
-        public async Task ProcessFile_ReturnsOk_WhenFileIsProcessedSuccessfully()
-        {
-            // Arrange
-            var fileName = "test.txt";
-            var fileContent = "2021-01-02_21:33:21;4";
-            var authorName = "Test Author";
-            var ms = new MemoryStream();
-            var writer = new StreamWriter(ms);
-            writer.Write(fileContent);
-            writer.Flush();
-            ms.Position = 0;
-
-            _mockFormFile.Setup(f => f.FileName).Returns(fileName);
-            _mockFormFile.Setup(_ => _.OpenReadStream()).Returns(ms);
-            _mockFormFile.Setup(_ => _.Length).Returns(ms.Length);
-
-            // Act
-            var result = await _controller.ProcessFile(_mockFormFile.Object, authorName);
-
-            // Assert
-            Assert.IsType<OkResult>(result);
-        }
-
-        [Fact]
         public async Task ProcessFile_ReturnsBadRequest_WhenFileIsNull()
         {
-            // Arrange
             IFormFile nullFile = null;
             var authorName = "Test Author";
-
-            // Act
             var result = await _controller.ProcessFile(nullFile, authorName);
-
-            // Assert
             var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
             Assert.Equal("Файл не загружен", badRequestResult.Value);
         }
-
-        // Additional test for the scenario when ProcessFileAsync throws an exception
+        [Fact]
+        public async Task GetResultsByQueryParams_ReturnsBadRequest_ForNoParameters()
+        {
+            var result = await _controller.GetResultsByQueryParams(null, null, null, null, null);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetResultsByQueryParams_ReturnsBadRequest_ForRange()
+        {
+            var result = await _controller.GetResultsByQueryParams(null, 10, null, null, null);
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+        [Fact]
+        public async Task GetResultsByQueryParams_ReturnsNotFound_WhenNoResults()
+        {
+            _mockExperimentService.Setup(s => s.GetResultsByQueryParams(It.IsAny<string>(), It.IsAny<double?>(), It.IsAny<double?>(), It.IsAny<int?>(), It.IsAny<int?>()))
+                                  .ReturnsAsync(new List<Result>());
+            var result = await _controller.GetResultsByQueryParams("test.txt", 10, 20, 5, 10);
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
     }
 }
